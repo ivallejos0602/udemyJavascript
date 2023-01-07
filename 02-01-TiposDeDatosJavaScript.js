ed7e0ca0be2b52f6cea04168e8152d79a11689b0
ivallejos0602 <ignvallejos@gmail.com>
2023-01-07 14:27:23 -0300

    1426 agregue un comentario antes de declarar el objeto

--- 02-01-TiposDeDatosJavaScript.js
+++ 02-01-TiposDeDatosJavaScript.js
@@ -1,6 +1,8 @@
 var numero =  1000;
 console.log(numero);
 
+// objeto sería "es una estructura creada por el usuario"
+
 var objeto = { nombre : "Oscar",
               apellido:"Tabarez",
             sueldo: 345000 }
