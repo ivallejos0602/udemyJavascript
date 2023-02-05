5d2a03ef1004dc764387eb2890726d90991a1e6e
ivallejos0602 <ignvallejos@gmail.com>
2023-02-05 10:11:03 -0300

    ejercicio sobre objetos, Creación e impresión por sonsola

---
+++ 08-62-objetos.js
@@ -0,0 +1,16 @@
+let persona = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    email: 'jperez@gmail.com',
+    edad: 28
+}
+
+console.log("nombre :"+persona.nombre);
+console.log("apellido :"+persona.apellido);
+console.log("edad :"+persona.edad);
+
+// imprimimos por consola el objeto completo :
+
+console.log("Objeto Persona : ");
+console.log(persona);
+
